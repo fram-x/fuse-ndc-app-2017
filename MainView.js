caca25cb1ded0ccac4c24c524907b3904c63bf1d
chrisva <christer@sercon.no>
2017-06-15 22:18:25 +0200

    Complete app

--- MainView.js
+++ MainView.js
@@ -2,33 +2,35 @@ var Observable = require('FuseJS/Observable');
 
 var boxesJson = [{
     "id": 1,
-    "text": "Hello 1",
+    "text": "Hello",
     "startColor": "#000",
     "endColor": "#EE5458"
   },
   {
     "id": 2,
-    "text": "Hello 2",
+    "text": "Fuse",
     "startColor": "#000",
     "endColor": "#8591a5"
   },
   {
     "id": 3,
-    "text": "Hello 3",
+    "text": "at",
     "startColor": "#000",
     "endColor": "#4aaf5d"
   },
   {
     "id": 4,
-    "text": "Hello 4",
+    "text": "NDC",
     "startColor": "#000",
     "endColor": "#EEaa58"
   }
 ];
 
+var sliderValue = Observable(0);
 var boxes = Observable();
 boxes.replaceAll(boxesJson);
 
 module.exports = {
-  boxes: boxes
+  boxes: boxes,
+  sliderValue: sliderValue
 };
