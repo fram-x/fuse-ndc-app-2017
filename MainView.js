a91af2afe7379a11607d8ee36f8df26bb134d9ea
chrisva <christer@sercon.no>
2017-06-15 21:23:11 +0200

    Ok complete

--- MainView.js
+++ MainView.js
@@ -2,19 +2,27 @@ var Observable = require('FuseJS/Observable');
 
 var boxesJson = [{
     "id": 1,
-    "text": "Hello 1"
+    "text": "Hello 1",
+    "startColor": "#000",
+    "endColor": "#EE5458"
   },
   {
     "id": 2,
-    "text": "Hello 2"
+    "text": "Hello 2",
+    "startColor": "#000",
+    "endColor": "#8591a5"
   },
   {
     "id": 3,
-    "text": "Hello 3"
+    "text": "Hello 3",
+    "startColor": "#000",
+    "endColor": "#4aaf5d"
   },
   {
     "id": 4,
-    "text": "Hello 4"
+    "text": "Hello 4",
+    "startColor": "#000",
+    "endColor": "#EEaa58"
   }
 ];
 
